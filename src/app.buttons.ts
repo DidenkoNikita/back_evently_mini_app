70749ce67f621ba91741c94a0c08c538488addf3
DidenkoNikita <n.didenko@redlight.group>
2024-05-22 17:20:51 +0300

    test

--- src/app.buttons.ts
+++ src/app.buttons.ts
@@ -1,21 +1,19 @@
-import { Markup } from "telegraf";
+import { Markup } from 'telegraf';
 
-export function actionButtons (){
+export function actionButtons() {
   return Markup.keyboard(
     [
-      Markup.button.webApp('🚀 Go to dating', process.env.MINI_APP_URL ),
+      Markup.button.webApp('🚀 Go to dating', process.env.MINI_APP_URL),
       Markup.button.webApp('👤 Edit profile', process.env.MINI_APP_URL),
-    ],  
+    ],
     {
-      columns: 3
-    }
-  )
+      columns: 3,
+    },
+  );
 }
 
 export function registerButton() {
-  return Markup.inlineKeyboard(
-    [
-      Markup.button.webApp('🙄 Sign up', `${process.env.MINI_APP_URL}/sign_up`)
-    ]
-  );
+  return Markup.inlineKeyboard([
+    Markup.button.webApp('💩 Sign up', `${process.env.MINI_APP_URL}/sign_up`),
+  ]);
 }
